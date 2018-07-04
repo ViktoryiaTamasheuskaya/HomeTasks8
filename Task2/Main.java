cc4e6d0fc22716f2d9896d748590aa1d762832c6
ViktoryiaTamasheuskaya <39959716+ViktoryiaTamasheuskaya@users.noreply.github.com>
2018-07-04 17:34:35 +0300

    Create Main.java

---
+++ Task2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Payment payment = new Payment();
+
+        payment.addItems();
+        payment.printCheck();
+
+    }
+}
